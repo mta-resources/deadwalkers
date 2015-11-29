fb18eb5f344fa2d238c0b1661831fd727327dcd3
Stanley Sathler <Stanley Sathler>
2015-11-29 10:15:02 -0200

    Added sublime-files and initializer script

--- deadwalkers/configuration/ParserXML.lua
+++ deadwalkers/configuration/ParserXML.lua
@@ -40,7 +40,3 @@ function allocItems()
 end
 addEvent("parserXml", true)
 addEventHandler("parserXml", resourceRoot, allocItems)
-
----------------------------------------------------------------
---
----------------------------------------------------------------
